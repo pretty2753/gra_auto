6fdaec7f0ae8f57517cc426dc603f8aa771c1fba
leesk0007 <leesk0007@naver.com>
2026-06-09 22:37:01 +0900

    [CHORE]: 기존 레거시 코드 및 불필요한 설정 파일 정리

--- terraform/modules/nat-gateway/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "nat_gateway_id" {
-  value = aws_nat_gateway.this.id
-}
-
-output "eip" {
-  value = aws_eip.this.public_ip
-}
--- terraform/modules/nat-gateway/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "subnet_id" {
-  description = "Public subnet ID where NAT Gateway will be deployed"
-  type        = string
-}
-
-variable "name" {
-  description = "Name tag for NAT Gateway"
-  type        = string
-}
