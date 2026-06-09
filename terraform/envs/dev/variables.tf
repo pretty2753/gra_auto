470972541cd301c5589c3b5987ebc61aa8d82a8b
leesk0007 <leesk0007@naver.com>
2026-06-09 22:37:01 +0900

    [FEAT]: Tailscale 연동 및 프로메테우스 동적 메트릭 수집 추가

--- terraform/envs/dev/variables.tf
+++ terraform/envs/dev/variables.tf
@@ -2,3 +2,9 @@ variable "ami_id" {
   type    = string
   default = null
 }
+
+variable "tailscale_auth_key" {
+  description = "Tailscale Auth Key for Subnet Router"
+  type        = string
+  sensitive   = true
+}
