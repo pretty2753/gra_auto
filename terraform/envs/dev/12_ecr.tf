f6c3e57927031d9153aa4a32b7fc5163ad27475c
leesk0007 <leesk0007@naver.com>
2026-06-11 17:28:40 +0900

    [FEAT]: DB 이미지를 Docker Hub에서 ECR로 이전

--- terraform/envs/dev/12_ecr.tf
+++ terraform/envs/dev/12_ecr.tf
@@ -15,3 +15,18 @@ resource "aws_ecr_repository" "was_repo" {
     Name = "project02-was-repo"
   }
 }
+
+# [2] DB 인스턴스용 ECR 리포지토리
+# → GitHub Actions 빌드 후 생성된 DB 커스텀 Docker 이미지를 저장하는 공간입니다.
+resource "aws_ecr_repository" "db_repo" {
+  name                 = "project02-db-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "project02-db-repo"
+  }
+}
